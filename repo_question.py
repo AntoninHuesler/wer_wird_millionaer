347127991f717d1e5ac44f68f7291985ca6fa323
Antonin <antonin.huesler@stud.hslu.ch>
2025-11-18 16:44:12 +0100

    Anpassen unklarer kommentare

--- repo_question.py
+++ repo_question.py
@@ -1,4 +1,5 @@
 """Datenbank-Operationen für Fragen im Spiel."""
+
 import sqlite3
 from pathlib import Path
 
@@ -7,8 +8,8 @@ db_questions_path = Path(__file__).resolve().parent / "data" / "questions.db"
 
 def pick(diff: str, n: int = 4):
     """Funktion holt n Fragen (Default = 4) einer Schwierigkeit (diff)"""
-    with sqlite3.connect(db_questions_path) as conn:  # verbindung zu questions.db
-        # Führt SQL Abfrage aus
+    with sqlite3.connect(db_questions_path) as conn:
+
         rows = conn.execute(
             """     
             SELECT text, answer_a, answer_b, answer_c, answer_d, correct_index
@@ -18,15 +19,15 @@ def pick(diff: str, n: int = 4):
             LIMIT ?;
         """,
             (diff, n),
-        ).fetchall()  # übergibt parameter diff & n den ?
+        ).fetchall()
     # baut aus jeder Zeile der db ein Tupel: (Fragetext, [Antwort A, Antwort B...], korrekterIndex)
     return [(r[0], [r[1], r[2], r[3], r[4]], r[5]) for r in rows]
 
 
 def fetch_12_questions():
     """führt die Funktion pick 4 mal aus mit den drei verschiedenen Schwierigkeiten."""
-    qs = pick("leicht", 4) + pick("mittel", 4) + pick("schwer", 4)
-    return qs
+    questions = pick("leicht", 4) + pick("mittel", 4) + pick("schwer", 4)
+    return questions
 
 
 if __name__ == "__main__":
